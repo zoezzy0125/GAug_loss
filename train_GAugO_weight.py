c8029175dbff567b184704fdc6ae7ab87e907430
zoezzy0125 <zoezzy0125@gmail.com>
2023-12-19 18:46:30 +0800

    minor change

--- train_GAugO_weight.py
+++ train_GAugO_weight.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='single')
     parser.add_argument('--dataset', type=str, default='cora')
     parser.add_argument('--gnn', type=str, default='gcn')
-    parser.add_argument('--gpu', type=str, default='0')
+    parser.add_argument('--gpu', type=int, default='0')
     parser.add_argument('--fidF', type=float, default='0') #description='fidelity fraction'
     parser.add_argument('--save_index', type=str, default='test_acc') 
     args = parser.parse_args()
@@ -20,18 +20,18 @@ if __name__ == "__main__":
     if args.gpu == '-1':
         gpu = -1
     else:
-        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
-        gpu = 0
+        #os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+        gpu = args.gpu
         
     
-    tvt_nids = pickle.load(open(f'/home/zzy/GAug/data/graphs/{args.dataset}_tvt_nids.pkl', 'rb'))
-    adj_orig = pickle.load(open(f'/home/zzy/GAug/data/graphs/{args.dataset}_adj.pkl', 'rb'))
-    features = pickle.load(open(f'/home/zzy/GAug/data/graphs/{args.dataset}_features.pkl', 'rb')).cpu()
-    labels = pickle.load(open(f'/home/zzy/GAug/data/graphs/{args.dataset}_labels.pkl', 'rb')).cpu()
+    tvt_nids = pickle.load(open(f'data/graphs/{args.dataset}_tvt_nids.pkl', 'rb'))
+    adj_orig = pickle.load(open(f'data/graphs/{args.dataset}_adj.pkl', 'rb'))
+    features = pickle.load(open(f'data/graphs/{args.dataset}_features.pkl', 'rb')).cpu()
+    labels = pickle.load(open(f'data/graphs/{args.dataset}_labels.pkl', 'rb')).cpu()
     if sp.issparse(features):
         features = torch.FloatTensor(features.toarray())
-    edge_weights = pickle.load(open(f'/home/zzy/GAug/{args.dataset}_deg_weight.pkl', 'rb'))
-    params_all = json.load(open('/home/zzy/GAug/best_parameters.json', 'r'))
+    edge_weights = pickle.load(open(f'{args.dataset}_deg_weight.pkl', 'rb'))
+    params_all = json.load(open('/best_parameters.json', 'r'))
     #params = params_all['GAugO'][args.dataset][args.gnn]
     params = params_all.get('GAugO', {}).get(args.dataset, params_all.get('GAugO', {}).get('cora', {}))
     
@@ -52,7 +52,7 @@ if __name__ == "__main__":
         n_layers = 3
 
     accs = []
-    total_big_epoch = 5
+    total_big_epoch = 10
     for big_epoch in range(total_big_epoch):
         model = GAug(adj_orig, features, labels, tvt_nids, cuda=gpu, 
                      gae=True, alpha=params['alpha'], beta=params['beta'], 
@@ -64,4 +64,4 @@ if __name__ == "__main__":
         acc = model.fit(pretrain_ep=params['pretrain_ep'], pretrain_nc=params['pretrain_nc'], save_index = args.save_index)
         accs.append(acc)
         print(f'Micro F1: {np.mean(accs):.6f}, std: {np.std(accs):.6f}, Max is {np.max(accs):.6f}, min is {np.min(accs):.6f}')
-        print("dataset is {} fid_frac is {}".format(args.dataset, args.fidF))
+        print("Big Epoch {}/{} dataset is {} fid_frac is {}".format(big_epoch, total_big_epoch, args.dataset, args.fidF))
